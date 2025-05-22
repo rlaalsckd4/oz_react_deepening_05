aeec9f836c1925149922d39545aa2b76f2818eea
rlaalsckd4 <kmc060901@gmail.com>
2025-05-22 16:42:27 +0900

    장바구니 완성하기 과제

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ const App = () => {
     const [cart, setCart] = useState([]);
 
     const addToCart = (product) => {
-        // TODO: 카트에 상품을 추가하는 함수를 완성하세요.
-        // 상품은 배열객체로 이루어져 있습니다.
-        // 카트에 상품이 8개 이상이면 alert를 띄워
-        // 더 이상 상품을 추가할 수 없다고 알려주세요.
+        if (cart.length >= 8){
+            alert("카트에 상품을 8개 이상 담을 수 없습니다.")
+            return
+        }
+        setCart([...cart,product])
     };
 
     return (
